f881c6300318d34bae0ed80f1d381f7d2f187998
Gerald Waga <106008160+geraldWaga92@users.noreply.github.com>
2023-03-07 09:54:51 -0800

    building header component

---
+++ components/Header.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image"
+import Link from "next/link"
+
+
+
+function Header() {
+  return (
+    <header className="flex items-center justify-between space-x-2 font-bold px-10 py-5">
+        <div className="flex items-center space-x-2">
+            <Link href="/">
+                <Image src="/logo.png"
+                    alt="logo"
+                    width ={50}
+                    height ={50}
+                    className="rounded-full"
+                    />
+            </Link>
+            <h1>Sougene's Daily Blog</h1>
+        </div>
+
+        <div></div>
+    </header>
+  )
+}
+
+export default Header
