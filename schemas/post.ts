6edbb6f7ae9d79451bcef3841cd38488d01f643c
Gerald Waga <106008160+geraldWaga92@users.noreply.github.com>
2023-03-07 10:09:14 -0800

    building the blog list component

--- schemas/post.ts
+++ schemas/post.ts
@@ -11,6 +11,12 @@ export default defineType({
       type: 'string',
     }),
     defineField({
+      name: 'description',
+      description: 'Enter a short snippet for the blog...',
+      title: 'string',
+      type: 'string',
+    }),
+    defineField({
       name: 'slug',
       title: 'Slug',
       type: 'slug',
